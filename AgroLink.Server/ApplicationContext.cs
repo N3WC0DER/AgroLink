08b8fd35ed1156d377479480e35a524dbb561c71
Глафира Алексенцева <aleksentseva@i-teco.ru>
2025-03-12 22:04:49 +0400

    добила ApplicationContext

--- AgroLink.Server/ApplicationContext.cs
+++ AgroLink.Server/ApplicationContext.cs
@@ -5,16 +5,24 @@ namespace AgroLink.Server
 {
     public class ApplicationContext : DbContext
     {
+        public ILogger logger { get; set; }
+        public DbSet<RegistrationRequest> RegistrationRequests { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+        public DbSet<Forwarder> Forwarders { get; set; }
         public DbSet<ExportRequest> ExportRequests { get; set; }
         public DbSet<ExportRequestDetails> ExportRequestDetails { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<ProductKind> ProductKinds { get; set; }
+        public DbSet<Truck> Trucks { get; set; }
 
         public ApplicationContext(DbContextOptions<ApplicationContext> options)
         : base(options)
         {
             Database.EnsureCreated();
-            // jrehfgerj
-            // testt
+            
         }
+        //проверить метод ApplicationContext
     }
 }
